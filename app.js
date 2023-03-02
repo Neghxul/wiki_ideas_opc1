049fb93bbc507927485092d2d89c392137186878
Neghxul <cgrr29us@gmail.com>
2023-03-02 00:38:29 -0600

    first commit

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 var imgCategory = document.querySelectorAll('.category img');
 var subCategory = document.querySelectorAll('.category .subtitle');
+var searchBar = document.querySelector('#search-text');
+var body = document.querySelector('body');
 var subValue = "";
+var barActive = false;
 
 subCategory.forEach((subtitle, index) => {
     subtitle.addEventListener('mouseover', () => {
         imgCategory[index].style.transform = "scale(1.15)";
         subtitle.style.opacity = "0.3";
+        subtitle.style.cursor = "pointer";
         subValue = subtitle.innerHTML;
         subtitle.innerHTML = "";
     });
@@ -15,4 +19,12 @@ subCategory.forEach((subtitle, index) => {
         subtitle.style.opacity = "1";
         subtitle.innerHTML = subValue;
     });
+});
+
+document.addEventListener('mouseup', (e) => {
+    if(!searchBar.contains(e.target)) {
+        searchBar.style.background = "#aaa";
+    } else {
+        searchBar.style.background = "#fff";
+    }
 });
